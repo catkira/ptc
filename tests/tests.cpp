72a1f5a9aca669eb5f15ea6e8f1af2b55819b704
benjamin menkuec <benjamin@menkuec.de>
2015-10-26 10:43:12 +0100

    more tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -18,10 +18,10 @@ unsigned int fibonacci(unsigned int n) {
 int main()
 {
     const unsigned int numThreads = 10;
-    vector<int> vals = {10,20,30,40,50,20,30,40};
-
-    unsigned int count = 0;
-    auto myptc = ptc::ordered_ptc(
+    
+    vector<int> vals = {10,20,30,40,42,20,30,40};
+    cout << "ordered" << endl;
+    auto myoptc = ptc::ordered_ptc(
         // produce
         [&vals]()->auto {
             if(vals.empty())
@@ -31,13 +31,35 @@ int main()
             return ret;
             },
         // transform
-        [](auto in)->auto {return make_unique<std::string>(std::to_string(*in) + " -> " + std::to_string(fibonacci(*in)));},
+        [](auto in)->auto {return make_unique<std::string>("fib(" + std::to_string(*in) + ") = " + std::to_string(fibonacci(*in)));},
+        // consume
+        [](auto in) {cout << *in << endl;},
+        numThreads
+        );
+    myoptc->start();
+    myoptc->wait();
+
+    vals = { 10,20,30,40,42,20,30,40 };
+    cout << "unordered" << endl;
+    auto myptc = ptc::unordered_ptc(
+        // produce
+        [&vals]()->auto {
+        if (vals.empty())
+            return unique_ptr<int>();
+        auto ret = make_unique<int>(vals.back());
+        vals.pop_back();
+        return ret;
+    },
+        // transform
+        [](auto in)->auto {return make_unique<std::string>("fib(" + std::to_string(*in) + ") = " + std::to_string(fibonacci(*in)));},
         // consume
         [](auto in) {cout << *in << endl;},
         numThreads
         );
     myptc->start();
     myptc->wait();
+
+
     cout << "hello world." << endl;
 }
 
